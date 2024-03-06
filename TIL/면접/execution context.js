55af5c5b85b88257ffc7a81e328b38af20862684
sangjun1126 <jeongsangjun1234@gmail.com>
2024-03-06 16:46:37 +0900

    호이스팅과 실행 컨텍스트 공부하기

---
+++ TIL/면접/execution context.js
@@ -0,0 +1,9 @@
+var temp = 'temp';
+function b() {
+    console.log('ㅎㅇ');
+}
+
+function a() {
+    b();
+}
+a();
---
+++ TIL/면접/hoisting.js
@@ -0,0 +1,11 @@
+// var foo1 = 1;
+// {
+//     console.log(foo1);
+//     let foo = 2;
+// }
+
+
+console.log(text);
+text = 'hello';
+var text
+console.log(text)
