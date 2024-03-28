afd213c52ffe14ca9878358400ca2c9877187fa7
sangjun1126 <jeongsangjun1234@gmail.com>
2024-03-28 17:15:31 +0900

    props와 state 맛보기

---
+++ TIL/면접/counterCom.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+function CounterComponent() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+    </div>
+  );
+}
+
+export default CounterComponent;
---
+++ TIL/면접/props.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ChildComponent from './ChildComponent';
+
+function ParentComponent() {
+  const message = "Hello from Parent!";
+  
+  return (
+    <div>
+      <ChildComponent message={message} />
+    </div>
+  );
+}
+
+export default ParentComponent;
